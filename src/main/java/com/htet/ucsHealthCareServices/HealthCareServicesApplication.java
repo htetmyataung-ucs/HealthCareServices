e6669f0489b5e120d17705433898161c33d5b2cd
htetmyataunge-ucs <htetmyataung.ucs@gmail.com>
2020-09-04 21:02:57 +0630

    healthcareservices

---
+++ src/main/java/com/htet/ucsHealthCareServices/HealthCareServicesApplication.java
@@ -0,0 +1,13 @@
+package com.htet.ucsHealthCareServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareServicesApplication.class, args);
+	}
+
+}
